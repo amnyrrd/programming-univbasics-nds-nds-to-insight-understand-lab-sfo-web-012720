3d3abccb2020d83c01e002a81d3d292d7d201986
Aaron Minyard <amnyrrd@gmail.com>
2020-01-15 23:17:44 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -7,7 +7,7 @@ def print_first_directors_movie_titles
   row_index = 0
   while row_index < directors_database.length do
       column_index = 0
-      while column_index < directors_database[row_index][:name][:movies].length do
+      while column_index < directors_database[row_index][:movies].length do
           if directors_database[row_index][:name].include?("Stephen Spielberg")
             puts directors_database[row_index][:movies][column_index][:title]
           end
